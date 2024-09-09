4f71e077c8ce516e5068e5e2ba0209c2a0b82da8
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-09 18:40:56 -0400

    feat: add the validation schemas to routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
+
 import { login, logout, register, profile } from '../controllers/auth.controllers.js'
 import { validToken } from '../middleware/validateToken.js'
+import { validateSchema } from '../middleware/dataValidation.js'
+import { loginSchema, registerSchema } from '../schema/auth.schema.js'
 
 const router = Router()
 
-router.post('/login', login)
-router.post('/register', register)
+router.post('/login', validateSchema(loginSchema), login)
+router.post('/register', validateSchema(registerSchema), register)
 router.post('/logout', validToken, logout)
 router.get('/profile', validToken, profile)
 
