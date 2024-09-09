97085510322d17a1d711d26aae6b65539ce03497
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-09 13:19:37 -0400

    feat: add getTasks controller (not tested)

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,7 +1,21 @@
 import Blog from '../models/blog.model.js'
 
+// get self posts
+export const getPosts = async (request, response) => {
+  const { id } = request.user
+  try {
+    const posts = await Blog.find().sort({ createdAt: -1, createdByID: id })
+    response.json(posts)
+  } catch (error) {
+    response.status(500).json({ message: 'Error fetching posts' })
+  }
+}
+
+// get specific post
 export const getPost = async (request, response) => {}
-export const getPosts = async (request, response) => {}
+// create a post
 export const addPost = async (request, response) => {}
+// delete a post
 export const deletePost = async (request, response) => {}
+// edit a post
 export const updatePost = async (request, response) => {}
