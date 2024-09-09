1e620e4c195d573c914e3f52fe119ec794de65cf
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-09 02:52:44 -0400

    feat: add validation middleware to routes of auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { login, logout, register, profile } from '../controllers/auth.controllers.js'
+import { validToken } from '../middleware/validateToken.js'
 
 const router = Router()
 
 router.post('/login', login)
 router.post('/register', register)
-router.post('/logout', logout)
-router.get('/profile', profile)
+router.post('/logout', validToken, logout)
+router.get('/profile', validToken, profile)
 
 export default router
