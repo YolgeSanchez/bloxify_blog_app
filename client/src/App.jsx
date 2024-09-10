8fba6b27cd8f9849076f19cdc59a6b305a767713
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-10 03:48:10 -0400

    feat: create login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/register" element={<h1>register</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/posts" element={<h1>Posts</h1>} />
         <Route path="/addPost" element={<h1>Add post</h1>} />
         <Route path="/posts/:id" element={<h1>Update post</h1>} />
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+function LoginPage() {
+  return (
+    <div className="loginPage">
+      <h1>Login</h1>
+    </div>
+  )
+}
+export default LoginPage
