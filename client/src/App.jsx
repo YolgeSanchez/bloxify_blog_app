87d4f3e415ce40b1f947affd483de66b9e105783
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-11 03:06:29 -0400

    feat: use authProvider in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/posts" element={<h1>Posts</h1>} />
-        <Route path="/addPost" element={<h1>Add post</h1>} />
-        <Route path="/posts/:id" element={<h1>Update post</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/posts" element={<h1>Posts</h1>} />
+          <Route path="/addPost" element={<h1>Add post</h1>} />
+          <Route path="/posts/:id" element={<h1>Update post</h1>} />
+          <Route path="/profile" element={<h1>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
