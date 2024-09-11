c5ea6814fef1d8febf8a2e88d297287a97bfe28f
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-11 19:41:07 -0400

    feat: create login page with functionality

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,44 @@
+import { useForm } from 'react-hook-form'
+
+import { useAuth } from '../context/AuthContext.jsx'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const { loginUser, isAuthenticated, errors: registerErrors } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuthenticated) navigate('/')
+  }, [isAuthenticated])
+
+  const onSubmit = handleSubmit(async (user) => {
+    await loginUser(user)
+  })
+
   return (
-    <div className="loginPage">
-      <h1>Login</h1>
+    <div className="registerPage">
+      <form onSubmit={onSubmit}>
+        {registerErrors.map((error, i) => (
+          <div className="error" key={i}>
+            {error}
+          </div>
+        ))}
+        <input type="email" {...register('email', { required: true })} placeholder="E-mail" />
+        {errors.email && <p>E-mail is required</p>}
+        <input
+          type="password"
+          {...register('password', { required: true })}
+          placeholder="Password"
+        />
+        {errors.password && <p>Password is required</p>}
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
