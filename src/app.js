78100d0c37d0b9bd5fea2e2310fbc7ba2ef059e5
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-10 21:03:48 -0400

    feat: change app.js to use cors package

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser'
 import e from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import authRoutes from './routes/auth.routes.js'
 import postsRoutes from './routes/posts.routes.js'
@@ -8,6 +9,7 @@ import postsRoutes from './routes/posts.routes.js'
 const app = e()
 
 // basic middleware
+app.use(cors())
 app.use(e.json())
 app.use(morgan('dev'))
 app.use(cookieParser())
