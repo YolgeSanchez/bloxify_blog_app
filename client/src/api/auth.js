dd944292599cc7744511be2c53f23e7b8ddfcbd2
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-11 19:38:17 -0400

    feat: add api login call

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
 const API = 'http://localhost:3000/api'
+
 const register = `${API}/register`
+const login = `${API}/login`
 
 export const registerCall = (user) => axios.post(register, user)
+export const loginCall = (user) => axios.post(login, user)
