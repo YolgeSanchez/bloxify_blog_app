48eed682d2b359a6433adfbf7f7a0bfacfd52d62
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-11 03:04:32 -0400

    fix: redirect if everything goes right and catch errors on fetch

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,15 +1,35 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useContext } from 'react'
 import { registerCall } from '../api/auth.js'
 
 export const AuthContext = createContext()
 
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be use within an AuthProvider')
+  }
+  return context
+}
+
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-  const register = async (user) => {
-    const result = await registerCall(user)
-    setUser(result)
+  const registerUser = async (user) => {
+    try {
+      const result = await registerCall(user)
+      console.log(result)
+      setUser(result)
+      setIsAuthenticated(true)
+    } catch (error) {
+      console.log({ error })
+    }
   }
 
-  return <AuthContext.Provider value={{ register, user }}>{children}</AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={{ registerUser, user, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
