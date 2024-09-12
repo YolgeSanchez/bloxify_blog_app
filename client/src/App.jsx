56164316d83a2a8f50813ffbb2ebe83f87a3bee6
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-12 12:56:31 -0400

    feat: add protected route component to App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 
-import RegisterPage from './pages/RegisterPage'
-import LoginPage from './pages/LoginPage'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 import HomePage from './pages/HomePage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import PostFormPage from './pages/PostFormPage.jsx'
@@ -12,13 +13,15 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/posts" element={<h1>Posts</h1>} />
-          <Route path="/addPost" element={<PostFormPage />} />
-          <Route path="/posts/:id" element={<PostFormPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/posts" element={<h1>Posts</h1>} />
+            <Route path="/addPost" element={<PostFormPage />} />
+            <Route path="/posts/:id" element={<PostFormPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
