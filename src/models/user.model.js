d0285b31f4e58d23a3180db8ac7d0d097ce88304
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-11 21:09:13 -0400

    feat: add likedBlogs field to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  likedBlogs: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Blog',
+    default: [],
+  },
 })
 
 export default mongoose.model('User', userSchema)
