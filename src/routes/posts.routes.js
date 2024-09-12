ab458fe4a941a210ab7fc294ff0745ed22b2759b
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-11 21:55:02 -0400

    feat: add like/unlike route to posts routes and export its function

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -7,6 +7,7 @@ import {
   addPost,
   deletePost,
   updatePost,
+  changeLike,
 } from '../controllers/posts.controllers.js'
 import { validateSchema } from '../middleware/dataValidation.js'
 import { createBlogSchema } from '../schema/blog.schema.js'
@@ -15,6 +16,7 @@ const router = Router()
 
 router.get('/posts', validToken, getPosts)
 router.get('/posts/:id', validToken, getPost)
+router.put('/posts/:id/like', validToken, changeLike)
 router.post('/posts', validToken, validateSchema(createBlogSchema), addPost)
 router.delete('/posts/:id', validToken, deletePost)
 router.put('/posts/:id', validToken, updatePost)
