8f870014860fca6de87bb29d0b3972f464fe5aaa
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-12 22:37:38 -0400

    feat: add verify call with axios

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,3 +2,4 @@ import axios from './axios.js'
 
 export const registerCall = (user) => axios.post('/register', user)
 export const loginCall = (user) => axios.post('/login', user)
+export const verifyToken = () => axios.get('/verify')
