870f9fde918c973db1e7a0cc667c46b76d962ee2
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-12 22:43:35 -0400

    feat: add /verify route to authentication routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { login, logout, register, profile } from '../controllers/auth.controllers.js'
+import { login, logout, register, profile, verifyToken } from '../controllers/auth.controllers.js'
 import { validToken } from '../middleware/validateToken.js'
 import { validateSchema } from '../middleware/dataValidation.js'
 import { loginSchema, registerSchema } from '../schema/auth.schema.js'
@@ -10,6 +10,7 @@ const router = Router()
 router.post('/login', validateSchema(loginSchema), login)
 router.post('/register', validateSchema(registerSchema), register)
 router.post('/logout', validToken, logout)
+router.get('/verify', verifyToken)
 router.get('/profile', validToken, profile)
 
 export default router
