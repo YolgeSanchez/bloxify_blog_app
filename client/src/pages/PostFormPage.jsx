08dc9aead16c8341bdedd5112b719883682fad5a
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-13 02:19:15 -0400

    feat: add the base forms of the post creation page

--- client/src/pages/PostFormPage.jsx
+++ client/src/pages/PostFormPage.jsx
@@ -1,9 +1,20 @@
+import { useForm } from 'react-hook-form'
+
 function PostFormPage() {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = handleSubmit((data) => {
+    // Send data to the server
+    console.log(data)
+  })
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Content" />
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title">Title</label>
+        <input type="text" {...register('title')} autoComplete="off" autoFocus />
+        <label htmlFor="description">Content</label>
+        <textarea placeholder="Content" {...register('description')} autoComplete="off" />
         <button type="submit">Submit</button>
       </form>
     </div>
