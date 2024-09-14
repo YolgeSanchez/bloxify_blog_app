48e2175abf7394543c3cb60f3207b8cad7142bd7
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-13 21:39:34 -0400

    feat: show posts info in profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,33 +1,52 @@
 import { useEffect, useState } from 'react'
 import { useAuth } from '../context/AuthContext.jsx'
+import { usePost } from '../context/PostContext.jsx'
 
 function ProfilePage() {
   const { getProfile } = useAuth()
+  const { getPosts } = usePost()
   const [profile, setProfile] = useState({})
+  const [posts, setPosts] = useState([])
 
   useEffect(() => {
     async function fetchProfile() {
       const response = await getProfile()
-      console.log(response)
       setProfile(response)
     }
+    async function fetchPosts() {
+      const response = await getPosts()
+      setPosts(response)
+      console.log(response)
+    }
     fetchProfile()
+    fetchPosts()
   }, [])
 
   return (
     <div className="profile-page">
-      <h1>Profile</h1>
-      <p>Your profile details will be displayed here.</p>
-      {
-        Object.keys(profile).length > 0 && (
+      <div className="profile">
+        <h1>Profile</h1>
+        <p>Your profile details will be displayed here.</p>
+        {Object.keys(profile).length > 0 && (
           <>
             <p>Username: {profile.username}</p>
             <p>Email: {profile.email}</p>
             <p>Likes: {profile.likes.length}</p>
             <p>Posts: {profile.posts}</p>
           </>
-        )
-      }
+        )}
+      </div>
+      <div className="posts">
+        {posts.map(({ _id, title, description, createdAt, user, likedBy }) => (
+          <div key={_id} className="post">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <p>Posted by: {user.username}</p>
+            <p>Liked by: {likedBy.length}</p>
+            <p>Created at: {createdAt}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
