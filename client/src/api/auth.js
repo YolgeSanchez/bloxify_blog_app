270c7a8443afd05059952244092609f54b5b7daa
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-13 20:27:45 -0400

    feat: add the profile axios call in auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,3 +3,6 @@ import axios from './axios.js'
 export const registerCall = (user) => axios.post('/register', user)
 export const loginCall = (user) => axios.post('/login', user)
 export const verifyToken = () => axios.get('/verify')
+
+// TODO: implement profile route by username /profile/:username
+export const profileCall = () => axios.get('/profile')
