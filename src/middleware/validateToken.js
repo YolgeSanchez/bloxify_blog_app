428dbbccdd9f321ac9aa2b3853333365c4ca1ccb
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-15 16:20:48 -0400

    fix: also validate if the token is empty or not

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -3,10 +3,10 @@ import { TOKEN_SECRET } from '../config.js'
 
 export const validToken = (request, response, next) => {
   const { token } = request.cookies
-  if (!token || token == '') return response.status(401).send('There is no token')
+  if (!token || token == '') return response.status(401).json(['There is no token'])
 
   jwt.verify(token, TOKEN_SECRET, (error, decoded) => {
-    if (error) return response.status(401).send('Invalid token')
+    if (error) return response.status(401).json(['Invalid token'])
     request.user = decoded
   })
 
