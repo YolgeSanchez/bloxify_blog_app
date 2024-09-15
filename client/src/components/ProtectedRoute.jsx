e04baa364960e5edd3e1b346a20ec8ce9f8fd3a1
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-15 16:30:34 -0400

    fix: protected route execute the outlet component when loading error fixed, now it does not

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -4,9 +4,8 @@ import { useAuth } from '../context/AuthContext.jsx'
 function ProtectedRoute() {
   const { isAuthenticated, loading } = useAuth()
 
-  if (!loading && !isAuthenticated) return <Navigate to="/login" replace />
-
-  return <Outlet />
+  if (!loading && !isAuthenticated) return <Navigate to="/login" />
+  if (!loading && isAuthenticated) return <Outlet />
 }
 
 export default ProtectedRoute
