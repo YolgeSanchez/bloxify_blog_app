7f03879fc3dbed246de595706319e230be0fde86
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-16 03:26:45 -0400

    feat: add a feed call to use in home route

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,4 +1,6 @@
 import axios from './axios.js'
 
+export const feedCall = () => axios.get('/feed')
+
 // TODO: get posts by username '/posts/:username'
 export const postsCall = () => axios.get('/posts')
