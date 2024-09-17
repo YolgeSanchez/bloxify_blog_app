e1b389f01d13ce095d08f177b166f0edacf2faa8
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-16 23:44:22 -0400

    fix: update the axios function in the file for auth calls

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,6 +3,4 @@ import axios from './axios.js'
 export const registerCall = (user) => axios.post('/register', user)
 export const loginCall = (user) => axios.post('/login', user)
 export const verifyToken = () => axios.get('/verify')
-
-// TODO: implement profile route by username /profile/:username
-export const profileCall = () => axios.get('/profile')
+export const profileCall = (user) => axios.get(`/profile/${user}`)
