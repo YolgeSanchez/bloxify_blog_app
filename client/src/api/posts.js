f31b26bedaa707dc42060a1cedd32383f43eae0c
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-17 04:42:17 -0400

    feat: update the axios function for posts to use a user param

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,6 +1,4 @@
 import axios from './axios.js'
 
+export const postsCall = (user) => axios.get(`/posts/${user}`)
 export const feedCall = () => axios.get('/feed')
-
-// TODO: get posts by username '/posts/:username'
-export const postsCall = () => axios.get('/posts')
