a6aba824f1552e0af93a7ab0428b85ac87772273
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-19 02:52:16 -0400

    feat: create all the posts api calls functions

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -2,3 +2,8 @@ import axios from './axios.js'
 
 export const postsCall = (user) => axios.get(`/posts/${user}`)
 export const feedCall = () => axios.get('/feed')
+export const postCall = (id) => axios.get(`/post/${id}`)
+export const likeCall = (id) => axios.put(`/posts/${id}/like`)
+export const addPostCall = (post) => axios.post('/posts/', post)
+export const removePostCall = (id) => axios.delete(`/posts/${id}`)
+export const updatePostCall = (id, post) => axios.put(`/posts/${id}`, post)
