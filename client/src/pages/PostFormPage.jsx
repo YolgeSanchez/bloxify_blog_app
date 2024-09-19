937fe41654f23c57dfd060c1f41d02251841c8c5
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-19 03:50:17 -0400

    feat: fill the inputs when updating and update the post on submit

--- client/src/pages/PostFormPage.jsx
+++ client/src/pages/PostFormPage.jsx
@@ -3,16 +3,33 @@ import { useForm } from 'react-hook-form'
 import { usePost } from '@/context/PostContext'
 import { useAuth } from '@/context/AuthContext'
 import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 function PostFormPage() {
-  const { addPost, updatePost } = usePost()
+  const { getPost, addPost, updatePost } = usePost()
   const { user } = useAuth()
   const { register, handleSubmit, setValue } = useForm()
   const params = useParams()
   const navigate = useNavigate()
 
+  useEffect(() => {
+    const fillInputs = async () => {
+      if (params.id) {
+        const post = await getPost(params.id)
+        setValue('title', post.title)
+        setValue('description', post.description)
+      }
+    }
+    fillInputs()
+  }, [])
+
   const onSubmit = handleSubmit(async (data) => {
     if (params.id) {
+      // TODO
+      const id = params.id
+      const response = await updatePost(id, data)
+      console.log(response)
+      navigate(`/profile/${user.username}`)
     } else {
       try {
         const response = await addPost(data)
