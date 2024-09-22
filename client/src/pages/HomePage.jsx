ebdd3fc74cffb0ddaab924cd6f1311df3f4a78be
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-22 04:29:15 -0400

    fix: set the responsive space for the home page with the navigation bar in

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -16,16 +16,18 @@ function HomePage() {
   }, [])
 
   return (
-    <div className="home-page">
+    <>
       <NavBar />
-      <h1>Home Page</h1>
-      <p>Welcome to the Blog App!</p>
-      <section className="posts">
-        {feed.map((post) => (
-          <Post post={post} key={post._id} />
-        ))}
-      </section>
-    </div>
+      <div className="pb-16 md:absolute md:left-48 md:top-0 md:w-[100% - 12rem] md:h-full">
+        <h1>Home Page</h1>
+        <p>Welcome to the Blog App!</p>
+        <section className="posts">
+          {feed.map((post) => (
+            <Post post={post} key={post._id} />
+          ))}
+        </section>
+      </div>
+    </>
   )
 }
 
