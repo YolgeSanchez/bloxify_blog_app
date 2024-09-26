e88642dd4b3dd6ba5dbf1cbd22d3ed031759337d
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-26 14:48:40 -0400

    feat: install dotenv and set the database connection string in .env file

--- src/db.js
+++ src/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
+import { configDotenv } from 'dotenv'
+configDotenv()
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect('mongodb://localhost/blogApp')
+    await mongoose.connect(process.env.mongoDBConnection)
     console.log('>>> DB is connected')
   } catch (error) {
     console.log(error)
