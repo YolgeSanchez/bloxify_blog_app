2a5618ad61f15e0a2eb83fb140a8d51e6637740a
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-28 14:40:35 -0400

    feat: implement __dirname variable like in commonjs

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import app from './src/app.js'
 import { PORT } from './src/config.js'
 import { dbConnect } from './src/db.js'
 
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+// Get the file path of the current module
+const __filename = fileURLToPath(import.meta.url)
+
+// Get the directory name of the current module
+export const __dirname = dirname(__filename)
+
 dbConnect()
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
