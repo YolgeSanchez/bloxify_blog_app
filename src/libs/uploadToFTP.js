0420b2bbcdca8929969dabf8771b62fe6fbd8a58
YolgeSanchez <imjorgesanchez@gmail.com>
2024-09-29 09:03:05 -0400

    feat: create the helper function to upload an image to the ftp server and return the http url to that image

---
+++ src/libs/uploadToFTP.js
@@ -0,0 +1,51 @@
+import { configDotenv } from 'dotenv'
+import ftp from 'basic-ftp'
+import path from 'path'
+import { __dirname } from '../../index.js'
+
+configDotenv()
+
+const uploadToFTP = async (user, file) => {
+  const client = new ftp.Client()
+  client.ftp.verbose = true
+
+  try {
+    // use the ftp credentials to make a connection
+    await client.access({
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PSSWD,
+      port: process.env.FTP_PORT,
+      secure: true, // Change this to true
+      secureOptions: {
+        // Add these options
+        rejectUnauthorized: false, // Only use this if you're using a self-signed certificate
+      },
+    })
+
+    // cannot set dir with path.join because it uses backslashes while ftp servers expects Unix-styles paths
+    const dir = `uploads/${user}`
+
+    // check if the user directory exists and after it get into the directory
+    try {
+      await client.ensureDir(dir)
+    } catch (error) {
+      return console.error('error ensuring dir', error)
+    }
+
+    // again make sure that remote path for ftp server uses forward slashes
+    const remotePath = `uploads/${user}/${file}` // this is the expected path in the ftp server
+    const localPath = path.join(__dirname, remotePath)
+
+    // upload the file to the ftp server
+    await client.uploadFrom(localPath, file)
+
+    // return http path to get the image on the web browser
+    const httpPath = `http://localhost:3000/api/${remotePath}`
+    return httpPath
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export default uploadToFTP
