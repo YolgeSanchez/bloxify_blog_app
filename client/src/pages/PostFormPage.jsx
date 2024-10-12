25be539902737b4118dc9d0f3020fe95fe1c436b
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-12 18:12:11 -0400

    feat: add 404 error handling in PostFormPage

--- client/src/pages/PostFormPage.jsx
+++ client/src/pages/PostFormPage.jsx
@@ -3,17 +3,19 @@ import { useForm } from 'react-hook-form'
 import { usePost } from '@/context/PostContext'
 import { useAuth } from '@/context/AuthContext'
 import { useParams, useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { Button } from '@/components/ui/button'
 import { Alert, AlertDescription } from '@/components/ui/alert'
 import { Label } from '@/components/ui/label'
+import NotFoundPage from '@/pages/NotFoundPage'
 
 function PostFormPage() {
   const { getPost, addPost, updatePost, errors: postErrors } = usePost()
   const { user } = useAuth()
+  const [notFound, setError] = useState(false)
   const {
     register,
     handleSubmit,
@@ -35,6 +37,10 @@ function PostFormPage() {
     }
     fillInputs()
   }, [])
+
+  useEffect(() => {
+    for (const error of postErrors) if (error == 'Not found') setError(true)
+  }, [postErrors])
 
   const onSubmit = handleSubmit(async (data) => {
     if (params.id) {
@@ -63,60 +69,66 @@ function PostFormPage() {
 
   return (
     <>
-      <NavBar />
-      <div className="pb-16 h-[calc(100vh-3rem)] flex items-center justify-center md:absolute md:left-48 md:top-0 md:mt-8 md:w-[calc(100%-12rem)] md:min-h-screen ">
-        <Card className="w-[85%] md:w-full max-w-2xl">
-          <CardHeader>
-            <CardTitle className="text-2xl">Create New Blog Post</CardTitle>
-          </CardHeader>
-          <form onSubmit={onSubmit}>
-            <CardContent className="space-y-6">
-              {postErrors.map((error, index) => (
-                <Alert variant="destructive" className="mb-4" key={index}>
-                  <AlertDescription className="md:text-lg">{error}</AlertDescription>
-                </Alert>
-              ))}
-              <div className="space-y-2">
-                <Label htmlFor="title" className="md:text-lg">
-                  Title
-                </Label>
-                <Input
-                  placeholder="Enter post title"
-                  {...register('title', { required: true })}
-                  autoComplete="off"
-                  className="md:text-md"
-                />
-                {errors.title && <p className="text-red-500 md:text-md">Title is required</p>}
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="description" className="md:text-lg">
-                  Description
-                </Label>
-                <Textarea
-                  placeholder="Enter post description"
-                  {...register('description', { required: true })}
-                  autoComplete="off"
-                  className="min-h-[100px] md:min-h-[160px] md:text-md"
-                />
-                {errors.description && (
-                  <p className="text-red-500 md:text-md">Description is required</p>
-                )}
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="image" className="md:text-lg">
-                  Image
-                </Label>
-                <Input type="file" {...register('image')} />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button type="submit" className="w-full">
-                Create Post
-              </Button>
-            </CardFooter>
-          </form>
-        </Card>
-      </div>
+      {notFound ? (
+        <NotFoundPage />
+      ) : (
+        <>
+          <NavBar />
+          <div className="pb-16 h-[calc(100vh-3rem)] flex items-center justify-center md:absolute md:left-48 md:top-0 md:mt-8 md:w-[calc(100%-12rem)] md:min-h-screen ">
+            <Card className="w-[85%] md:w-full max-w-2xl">
+              <CardHeader>
+                <CardTitle className="text-2xl">Create New Blog Post</CardTitle>
+              </CardHeader>
+              <form onSubmit={onSubmit}>
+                <CardContent className="space-y-6">
+                  {postErrors.map((error, index) => (
+                    <Alert variant="destructive" className="mb-4" key={index}>
+                      <AlertDescription className="md:text-lg">{error}</AlertDescription>
+                    </Alert>
+                  ))}
+                  <div className="space-y-2">
+                    <Label htmlFor="title" className="md:text-lg">
+                      Title
+                    </Label>
+                    <Input
+                      placeholder="Enter post title"
+                      {...register('title', { required: true })}
+                      autoComplete="off"
+                      className="md:text-md"
+                    />
+                    {errors.title && <p className="text-red-500 md:text-md">Title is required</p>}
+                  </div>
+                  <div className="space-y-2">
+                    <Label htmlFor="description" className="md:text-lg">
+                      Description
+                    </Label>
+                    <Textarea
+                      placeholder="Enter post description"
+                      {...register('description', { required: true })}
+                      autoComplete="off"
+                      className="min-h-[100px] md:min-h-[160px] md:text-md"
+                    />
+                    {errors.description && (
+                      <p className="text-red-500 md:text-md">Description is required</p>
+                    )}
+                  </div>
+                  <div className="space-y-2">
+                    <Label htmlFor="image" className="md:text-lg">
+                      Image
+                    </Label>
+                    <Input type="file" {...register('image')} />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button type="submit" className="w-full">
+                    Create Post
+                  </Button>
+                </CardFooter>
+              </form>
+            </Card>
+          </div>
+        </>
+      )}
     </>
   )
 }
