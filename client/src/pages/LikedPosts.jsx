32731730bff9eb1d8dad05aa311e7f9db47eec43
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-12 18:11:26 -0400

    feat: add error handling and 404 redirection in LikedPostsPage

--- client/src/pages/LikedPosts.jsx
+++ client/src/pages/LikedPosts.jsx
@@ -4,11 +4,13 @@ import { useAuth } from '@/context/AuthContext'
 import Post from '@/components/Post'
 import Profile from '@/components/Profile'
 import NavBar from '@/components/NavBar'
+import NotFoundPage from './NotFoundPage'
 
 function LikedPostsPage() {
-  const { getProfile } = useAuth()
+  const { getProfile, errors } = useAuth()
   const [profile, setProfile] = useState({})
   const [likedPosts, setLikes] = useState([])
+  const [profileErrors, setErrors] = useState([])
   const params = useParams()
 
   useEffect(() => {
@@ -24,18 +26,29 @@ function LikedPostsPage() {
     fetchProfile()
   }, [])
 
+  useEffect(() => {
+    if (errors.length !== 0) setErrors(errors)
+  }, [errors])
+
   return (
     <>
-      <NavBar />
-      <div className="pb-16 w-full md:absolute md:left-48 md:top-0 md:w-[calc(100%-12rem)] md:h-full flex flex-col items-center justify-start">
-        <div className="profile">
-          {Object.keys(profile).length > 0 && <Profile profile={profile} />}
-        </div>
-        <section className="posts">
-          {likedPosts.length == 0 && <p>No posts liked yet</p>}
-          {likedPosts.length > 0 && likedPosts.map((post) => <Post post={post} key={post._id} />)}
-        </section>
-      </div>
+      {profileErrors.length !== 0 ? (
+        <NotFoundPage />
+      ) : (
+        <>
+          <NavBar />
+          <div className="pb-16 w-full md:absolute md:left-48 md:top-0 md:w-[calc(100%-12rem)] md:h-full flex flex-col items-center justify-start">
+            <div className="profile">
+              {Object.keys(profile).length > 0 && <Profile profile={profile} />}
+            </div>
+            <section className="posts">
+              {likedPosts.length == 0 && <p>No posts liked yet</p>}
+              {likedPosts.length > 0 &&
+                likedPosts.map((post) => <Post post={post} key={post._id} />)}
+            </section>
+          </div>
+        </>
+      )}
     </>
   )
 }
